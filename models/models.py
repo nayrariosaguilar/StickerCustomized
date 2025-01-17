b08e5da7daf3c43b8889d9ba4f93c8645d591558
nayrariosaguilar <nayrios2004@gmail.com>
2025-01-17 16:58:42 +0100

    parte dificil

--- models/models.py
+++ models/models.py
@@ -38,3 +38,18 @@ class stickers_printing(models.Model):
     _name = 'stickers.printing'
     _description = "Type of printing"
     name = fields.Char(string="Type of printing", size=255, required=True, help='Type of printing')
+
+#Taula amb els films
+class stickers_shape(models.Model):
+    """materials for sticker"""
+    _name = 'stickers.shape'
+    _description = "Name of the shape"
+    id_finish  = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_color   = fields.Many2Many(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_scale   = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_shape   = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_material  = fields.Many2many(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    width  = fields.Integer(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    message_personalized  = fields.Text("Message", help="Personalized Message")
+    image_personalized  = fields.Text("Image", help="Description of the category")
+    height  = fields.Integer(string="Name of the shape", size=255, required=True, help='Name of the shape')
