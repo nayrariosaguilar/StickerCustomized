b5e734014eb9af317eca3e22936e2e9fae2c4235
nayrariosaguilar <nayrios2004@gmail.com>
2025-01-17 16:54:23 +0100

    problemas

--- models/models.py
+++ models/models.py
@@ -26,3 +26,17 @@ class stickers_shape(models.Model):
     _description = "Name of the shape"
     name  = fields.Char(string="Name of the shape", size=255, required=True, help='Name of the shape')
 
+#Taula amb els films
+class stickers_shape(models.Model):
+    """materials for sticker"""
+    _name = 'stickers.shape'
+    _description = "Name of the shape"
+    id_finish  = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_color   = fields.Many2Many(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_scale   = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_shape   = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_material  = fields.Many2many(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    width  = fields.Integer(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    message_personalized  = fields.Text("Message", help="Personalized Message")
+    image_personalized  = fields.Text("Image", help="Description of the category")
+    height  = fields.Integer(string="Name of the shape", size=255, required=True, help='Name of the shape')
