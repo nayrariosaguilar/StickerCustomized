e9b463e0dd972ffcedbacbc60c2b388c154a9d54
nayrariosaguilar <nayrios2004@gmail.com>
2025-01-17 17:05:40 +0100

    si

--- models/models.py
+++ models/models.py
@@ -19,12 +19,12 @@ class stickers_shape(models.Model):
     _description = "Name of the shape"
     name  = fields.Char(string="Name of the shape", size=255, required=True, help='Name of the shape')
 
-#Taula amb els films
-class stickers_shape(models.Model):
-    """materials for sticker"""
-    _name = 'stickers.shape'
-    _description = "Name of the shape"
-    name  = fields.Char(string="Name of the shape", size=255, required=True, help='Name of the shape')
+#Taula amb les color
+class stickers_color(models.Model):
+    """shape of the sticker"""
+    _name = 'stickers.color'
+    _description = "Name of the color"
+    color_name  = fields.Char(string="Name of the color", size=255, required=True, help='Name of the shape')
 
 #scale
 class stickers_scale(models.Model):
@@ -40,15 +40,15 @@ class stickers_printing(models.Model):
     name = fields.Char(string="Type of printing", size=255, required=True, help='Type of printing')
 
 #Taula amb els films
-class stickers_shape(models.Model):
+class stickers_customized(models.Model):
     """materials for sticker"""
-    _name = 'stickers.shape'
+    _name = 'stickers.customized'
     _description = "Name of the shape"
-    id_finish  = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
-    id_color   = fields.Many2Many(string="Name of the shape", size=255, required=True, help='Name of the shape')
-    id_scale   = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
-    id_shape   = fields.Many2One(string="Name of the shape", size=255, required=True, help='Name of the shape')
-    id_material  = fields.Many2many(string="Name of the shape", size=255, required=True, help='Name of the shape')
+    id_finish  = fields.Many2One('stickers.printing',string="Name of the shape", help='Name of the shape')
+    id_color   = fields.Many2Many('stickers.color',string="Name of the shape",  help='Name of the shape')
+    id_scale   = fields.Many2One('stickers.scale',string="Name of the shape",  help='Name of the shape')
+    id_shape   = fields.Many2One('stickers.shape',string="Name of the shape",  help='Name of the shape')
+    id_material  = fields.Many2many('stickers.material',string="Name of the shape",  help='Name of the shape')
     width  = fields.Integer(string="Name of the shape", size=255, required=True, help='Name of the shape')
     message_personalized  = fields.Text("Message", help="Personalized Message")
     image_personalized  = fields.Text("Image", help="Description of the category")
