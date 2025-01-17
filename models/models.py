d17e1baa4057944ad2db32fca70a0e704660c745
Arnau Nunez <arnaunl3@gmail.com>
2025-01-17 16:49:00 +0100

    dos tables

--- models/models.py
+++ models/models.py
@@ -26,3 +26,15 @@ class stickers_shape(models.Model):
     _description = "Name of the shape"
     name  = fields.Char(string="Name of the shape", size=255, required=True, help='Name of the shape')
 
+#scale
+class stickers_scale(models.Model):
+    """materials for sticker"""
+    _name = 'stickers.scale'
+    name  = fields.Char(string="Name of the scale", size=255, required=True, help='Name of the scale')
+
+#printing
+class stickers_printing(models.Model):
+    """materials for sticker"""
+    _name = 'stickers.printing'
+    _description = "Type of printing"
+    name = fields.Char(string="Type of printing", size=255, required=True, help='Type of printing')
