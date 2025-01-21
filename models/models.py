47bbef43e4a83823174cf071afa22089728ad913
nayri <nayrios2004@gmail.com>
2025-01-21 12:31:34 +0100

    si

--- models/models.py
+++ models/models.py
@@ -1,28 +1,47 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class StickersMaterial(models.Model):
     _name = 'stickers.material'
     _description = "Materials for stickers"
-    name = fields.Char(string="Material Name", size=255, required=True, help="Name of the material")
+
+    name = fields.Char(
+        string="Material Name",
+        size=50,
+        required=True,
+        help="Name of the material"
+    )
+
 
 class StickersShape(models.Model):
-        _name = 'stickers.shape'
-        _description = "Shapes available for stickers"
-        name = fields.Selection(
-            [("1", "Rectangle"), ("2", "Square"), ("3", "Circle"), ("4", "Star"), ("5", "Triangle"), ],
-            string="Selection")
+    _name = 'stickers.shape'
+    _description = "Shapes available for stickers"
+
+    name = fields.Selection(
+        [
+            ("1", "Rectangle"),
+            ("2", "Square"),
+            ("3", "Circle"),
+            ("4", "Star"),
+            ("5", "Triangle"),
+        ],
+        string="Shape",
+        required=True,
+        help="Available shapes for stickers"
+    )
+
 
 class StickersColor(models.Model):
     _name = 'stickers.color'
     _description = "Colors available for stickers"
-    color_name = fields.Char(string="Color Name", size=255, required=True, help="Name of the color", default="Blanco")
 
-class ColorCategory(models.Model):
-    _name = 'stickers.color.category'
-    _description = "Categories available for colors"
-    category_name = fields.Selection(
+    color_name = fields.Char(
+        string="Color Name",
+        size=100,
+        required=True,
+        help="Name of the color",
+        default="Blanco"
+    )
+    category = fields.Selection(
         [
             ('1', 'Rojos y Rosas'),
             ('2', 'Naranjas'),
@@ -35,35 +54,39 @@ class ColorCategory(models.Model):
             ('9', 'Negros'),
             ('10', 'Blancos'),
         ],
-        string="Finish Type",
+        string="Color Category",
         required=True,
-        help="Select the type of finish of your sticker"
+        help="Category to which this color belongs"
     )
-    id_color = fields.Many2one(
-        'stickers.color',
-        string="Color of the Sticker",
-        help="Choose the color of the Sticker"
-    )
+
 
 class StickersScale(models.Model):
     _name = 'stickers.scale'
     _description = "Scales available for stickers"
-    name = fields.Char(string="Scale Name", size=255, required=True, help="Name of the scale")
+
+    name = fields.Char(
+        string="Scale Name",
+        size=255,
+        required=True,
+        help="Name of the scale"
+    )
+
 
 class StickersPrinting(models.Model):
     _name = 'stickers.printing'
     _description = "Printing types available for stickers"
+
     name = fields.Selection(
         [
-            ('glossy', 'Glossy'),
-            ('matte', 'Matte'),
-            ('transparent', 'Transparent'),
-            ('embossed', 'Embossed'),
-            ('holographic', 'Holographic'),
+            ('1', 'Glossy'),
+            ('2', 'Matte'),
+            ('3', 'Transparent'),
+            ('4', 'Embossed'),
+            ('5', 'Holographic'),
         ],
-        string="Finish Type",
+        string="Printing Type",
         required=True,
-        help="Select the type of finish of your sticker"
+        help="Select the type of printing for the sticker"
     )
 
 
@@ -75,11 +98,6 @@ class StickersCustomized(models.Model):
         'stickers.printing',
         string="Printing Type",
         help="Type of printing for the sticker"
-    )
-    id_color = fields.Many2many(
-        'stickers.color',
-        string="Colors",
-        help="Colors used for the sticker"
     )
     id_scale = fields.Many2one(
         'stickers.scale',
@@ -99,25 +117,52 @@ class StickersCustomized(models.Model):
     id_product = fields.Many2one(
         'product.product',
         string="Product",
-        help="Materials used for the sticker"
+        help="Product associated with the sticker"
     )
     width = fields.Integer(
         string="Width",
         required=True,
         help="Width of the sticker"
     )
-
     height = fields.Integer(
         string="Height",
         required=True,
         help="Height of the sticker"
     )
-
     message_personalized = fields.Text(
-        "Personalized Message",
+        string="Personalized Message",
         help="Custom message for the sticker"
     )
-    image_personalized = fields.Text(
-       "Personalized Image",
-        help="Description of the custom image for the sticker"
+    image_personalized = fields.Image(
+        string="Image",
+        help="Upload an image for your sticker"
     )
+    category_filter_color = fields.Selection(
+        [
+            ('1', 'Rojos y Rosas'),
+            ('2', 'Naranjas'),
+            ('3', 'Amarillos'),
+            ('4', 'Verdes'),
+            ('5', 'Azules'),
+            ('6', 'Morados y Violetas'),
+            ('7', 'Marrones'),
+            ('8', 'Grises y Neutros'),
+            ('9', 'Negros'),
+            ('10', 'Blancos'),
+        ],
+        string="Color Category",
+        help="Filter colors by category"
+    )
+    id_color = fields.Many2many(
+        'stickers.color',
+        string="Colors",
+        help="Colors used for the sticker"
+    )
+
+    @api.onchange('category_filter_color')
+    def _onchange_category_filter_color(self):
+        """Filter colors based on the selected category."""
+        if self.category_filter_color:
+            return {'domain': {'id_color': [('category', '=', self.category_filter_color)]}}
+        else:
+            return {'domain': {'id_color': []}}
