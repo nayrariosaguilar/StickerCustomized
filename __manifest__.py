304ab3d0990db275763c468cd31aeab601837cf1
nayrariosaguilar <nayrios2004@gmail.com>
2025-01-27 17:14:39 +0100

    prueba!

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-    'icon': '/stickers_customization/static/description/icon.png',
+    'icon': '~/static/description/icon.png',
     # any module necessary for this one to work correctly
     'depends': ['base','sale','product'],
 
