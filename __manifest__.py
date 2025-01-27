b34ac7925e2db7ca2c319549a3a730c51a67d911
nayrariosaguilar <nayrios2004@gmail.com>
2025-01-27 16:54:54 +0100

    ahorsi2

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-    'icon': '/stickers_customization/static/description/sticker.png',
+    'icon': '/stickers_customization/static/description/icon.png',
     # any module necessary for this one to work correctly
     'depends': ['base','sale','product'],
 
