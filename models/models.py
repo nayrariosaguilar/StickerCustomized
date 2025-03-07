b8c9ef8479df78f89f9b88a966e3dcc00f91f718
nayrariosaguilar <nayrios2004@gmail.com>
2025-03-07 11:21:00 +0100

    done

--- models/models.py
+++ models/models.py
@@ -1,68 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-class StickersMaterial(models.Model):
-    _name = 'stickers.material'
-    _description = "Materials for stickers"
-    name = fields.Char(
-        string="Material Name",
-        size=50,
-        required=True,
-        help="Name of the material"
-    )
-
-
-class StickersShape(models.Model):
-    _name = 'stickers.shape'
-    _description = "Shapes available for stickers"
-
-name = fields.Char(
-    string="Shape",
-    size=20,
-    required=True,
-    help="Name of the shape"
-)
-
-def name_get(self):
-    result = []
-    for record in self:
-        # Combina el id y el nombre, o solo el nombre
-        result.append((record.id, record.name))
-    return result
-
-class StickersColor(models.Model):
-    _name = 'stickers.color'
-    _description = "Colors available for stickers"
-
-    color_name = fields.Char(
-        string="Color Name",
-        size=100,
-        required=True,
-        help="Name of the color",
-        default="Blanco"
-    )
-    category = fields.Selection(
-        [
-            ('1', 'Rojos y Rosas'),
-            ('2', 'Naranjas'),
-            ('3', 'Amarillos'),
-            ('4', 'Verdes'),
-            ('5', 'Azules'),
-            ('6', 'Morados y Violetas'),
-            ('7', 'Marrones'),
-            ('8', 'Grises y Neutros'),
-            ('9', 'Negros'),
-            ('10', 'Blancos'),
-        ],
-        string="Color Category",
-        required=True,
-        help="Category to which this color belongs"
-    )
-_sql_constraints = [
-    ('unique_material_name', 'UNIQUE(color_name)', 'The color name must be unique'),
-]
-
-
 class StickersScale(models.Model):
     _name = 'stickers.scale'
     _description = "Scales available for stickers"
@@ -73,36 +11,61 @@ class StickersScale(models.Model):
         help="Name of the scale"
     )
 
-    def name_get(self):
-        """Generic name_get method for models with a 'name' field."""
-        result = []
-        for record in self:
-            result.append((record.id, record.name))
-        return result
-
+class StickersShape(models.Model):
+    _name = 'stickers.shape'
+    _description = "Shapes available for stickers"
+    name = fields.Char(
+        string="Shape",
+        size=20,
+        required=True,
+        help="Name of the shape"
+    )
+    _sql_constraints = [
+        ('name_unique', 'UNIQUE(name)', 'The shape name must be unique!')
+    ]
 
 class StickersPrinting(models.Model):
     _name = 'stickers.printing'
     _description = "Printing types available for stickers"
     name = fields.Char(
-        string="Scale Name",
+        string="Printing Type",
         size=30,
         required=True,
-        help="Name of the scale"
+        help="Name of the printing type"
     )
 
-    def name_get(self):
-        """Generic name_get method for models with a 'name' field."""
-        result = []
-        for record in self:
-            result.append((record.id, record.name))
-        return result
+class StickersMaterial(models.Model):
+    _name = 'stickers.material'
+    _description = 'Materiales para fabricación de stickers'
 
+    product_template_id = fields.Many2one(
+        'product.template',
+        string="Material",
+        required=True,
+        ondelete='cascade'
+    )
+    main_supplier_id = fields.Many2one('res.partner', string="Proveedor principal")
+    cost_unit = fields.Float(string="Costo por unidad", help="Costo unitario del material")
+    last_purchase_date = fields.Date(string="Fecha de última compra")
+    stock = fields.Float(
+        string="Cantidad disponible (m²)",
+        compute="_compute_stock",
+        store=True
+    )
+    image_1920 = fields.Image(
+        string="Imagen del material",
+        related="product_template_id.image_1920",
+        readonly=False
+    )
+
+    @api.depends('product_template_id.qty_available')
+    def _compute_stock(self):
+        for material in self:
+            material.stock = material.product_template_id.qty_available
 
 class StickersCustomized(models.Model):
     _name = 'stickers.customized'
     _description = "Details of customized stickers"
-
     id_finish = fields.Many2one(
         'stickers.printing',
         string="Printing Type",
@@ -118,15 +81,15 @@ class StickersCustomized(models.Model):
         string="Shape",
         help="Shape of the sticker"
     )
-    id_material = fields.Many2many(
-        'stickers.material',
-        string="Materials",
-        help="Materials used for the sticker"
-    )
     id_product = fields.Many2one(
         'product.product',
         string="Product",
         help="Product associated with the sticker"
+    )
+    id_material = fields.Many2many(
+        'stickers.material',
+        string="Materials",
+        help="Materials used for the sticker"
     )
     width = fields.Integer(
         string="Width",
@@ -138,35 +101,17 @@ class StickersCustomized(models.Model):
         required=True,
         help="Height of the sticker"
     )
-    message_personalized = fields.Text(
-        string="Personalized Message",
-        help="Custom message for the sticker"
-    )
     image_personalized = fields.Image(
         string="Image",
         help="Upload an image for your sticker"
     )
-    category_filter_color = fields.Selection(
-        [
-            ('1', 'Rojos y Rosas'),
-            ('2', 'Naranjas'),
-            ('3', 'Amarillos'),
-            ('4', 'Verdes'),
-            ('5', 'Azules'),
-            ('6', 'Morados y Violetas'),
-            ('7', 'Marrones'),
-            ('8', 'Grises y Neutros'),
-            ('9', 'Negros'),
-            ('10', 'Blancos'),
-        ],
-        string="Color Category",
-        help="Filter colors by category"
-    )
     id_color = fields.Many2many(
-        'stickers.color',
-        string="Colors",
-        help="Colors used for the sticker"
+        'product.attribute.value',
+        string="Color",
+        domain="[('attribute_id.display_type', '=', 'color')]",
+        help="Select a color for the sticker"
     )
+
     @api.constrains('width')
     def _check_width_positive(self):
         for record in self:
@@ -179,10 +124,14 @@ class StickersCustomized(models.Model):
             if record.height <= 0:
                 raise ValidationError('The height must be greater than zero!')
 
-    @api.onchange('category_filter_color')
-    def _onchange_category_filter_color(self):
-        """Filter colors based on the selected category."""
-        if self.category_filter_color:
-            return {'domain': {'id_color': [('category', '=', self.category_filter_color)]}}
-        else:
-            return {'domain': {'id_color': []}}
+    @api.onchange('id_material')
+    def onchange_material(self):
+        if self.id_material:
+            for material in self.id_material:
+                if material.stock < 10:
+                    return {
+                        'warning': {
+                            'title': 'Stock Bajo',
+                            'message': f'El material {material.product_template_id.name} tiene pocas existencias ({material.stock} m²)'
+                        }
+                    }
